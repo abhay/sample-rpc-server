7c611df42b81851594c7f52561e458b9b97746e1
Abhay Kumar <abhay+git@opensynapse.net>
2008-05-29 13:23:27 -0700

    all we need are the beams from mochiweb

--- start-dev.sh
+++ start-dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl +Bc +K true -smp auto -pa $PWD/ebin $PWD/deps/*/ebin -boot start_sasl -name rpc_server -config conf/rpc_server -s reloader -s rpc_server
+exec erl +Bc +K true -smp auto -pa $PWD/ebin $PWD/deps/mochiweb -boot start_sasl -name rpc_server -config conf/rpc_server -s reloader -s rpc_server
